11cc10e3c3e875cd0b5d1e4a3aa48c2b0ac39395
yday <yangdayeon2@gmail.com>
2022-11-13 23:16:55 +0900

    이름, 체중, 키 입력

--- main.py
+++ main.py
@@ -6,14 +6,44 @@ class Routine:
     # 클래스 변수
     _ROUTINE = ('전신', '상체', '하체')   # 0: 전신, 1:상체, 2:하체
 
-    def __init__(self, name, weight, stature):
-        self.name = name    # 이름
-        self.weight = weight    # 체중
-        self.stature = stature  # 키
+    def __init__(self):
+        self.name = '없음'    # 이름
+        self.weight = 0    # 체중
+        self.stature = 0  # 키
         self.routine = 0    # 0:전신, 1:상체, 2:하체
 
     def __str__(self):
         return f'이름 : {self.name}\t키 : {self.stature}cm\t체중 : {self.weight}kg\t루틴 : {Routine._ROUTINE[self.routine]}'
+
+    def set_name(self):
+        # 사용자에게 이름 묻기
+        name = input('이름을 입력하세요 : ')
+        while True :
+            if name == '':  # 엔터 치면 다시 묻기
+                name = input('이름을 입력하세요 : ')
+            else:
+                self.name = name
+                break
+
+    def set_stature(self):
+        # 사용자에게 키 묻기
+        stature = input('키를 입력하세요 : ')
+        while True :
+            if stature == '':  # 엔터 치면 다시 묻기
+                stature = input('키를 입력하세요 : ')
+            else:
+                self.stature = stature
+                break
+
+    def set_weight(self):
+        # 사용자에게 키 묻기
+        weight = input('몸무게를 입력하세요 : ')
+        while True :
+            if weight == '':  # 엔터 치면 다시 묻기
+                weight = input('몸무게를 입력하세요 : ')
+            else:
+                self.weight = weight
+                break
 
     def set_routine(self):
         # 사용자에게 숫자 묻기 → 0:전신, 1:상체, 2:하체
@@ -26,9 +56,12 @@ class Routine:
             self.routine = int(routine)-1   # 숫자를 제대로 입력했을 때
 
     def order(self):
+        self.set_name()
+        self.set_stature()
+        self.set_weight()
         self.set_routine()
 
 if __name__ == '__main__':
-    member1 = Routine("양다연", 66, 161)
+    member1 = Routine()
     member1.order()
     print(member1)
