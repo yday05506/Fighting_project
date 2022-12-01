2f32d9e586e64c86b819c6518f9af332bad368d7
yday05506 <yangdayeon2@gmail.com>
2022-12-02 08:41:05 +0900

    세세한 부분 추가

--- calorie.py
+++ calorie.py
@@ -4,17 +4,17 @@ from main import Main
 
 
 class Calorie(Routine):
-    Korean_food = ('떡볶이', '튀김', '순대', '김밥', '어묵','X')
-    Korean_food_calorie = (500, 600, 700, 400, 300)
+    Korean_food = ('떡볶이 - 500kcal', '튀김 - 600kcal', '순대 - 700kcal', '김밥 - 400kcal', '어묵 - 300kcal','X')
+    Korean_food_calorie = (500, 600, 700, 400, 300, 0)
 
-    Western_food = ('피자', '스파게티', '햄버거', '치킨', '샐러드','X')
-    Western_food_calorie = (1000, 800, 700, 600, 300)
+    Western_food = ('피자 - 1000kcal', '스파게티 - 800kcal', '햄버거 - 700kcal', '치킨 - 600kcal', '샐러드 - 300kcal','X')
+    Western_food_calorie = (1000, 800, 700, 600, 300, 0)
 
-    Chinese_food = ('짜장면', '짬뽕', '탕수육', '볶음밥', '군만두','X')
-    Chinese_food_calorie = (700, 800, 1000, 600, 500)
+    Chinese_food = ('짜장면 - 700kcal', '짬뽕 - 800kcal', '탕수육 - 1000kcal', '볶음밥 - 600kcal', '군만두 - 500kcal','X')
+    Chinese_food_calorie = (700, 800, 1000, 600, 500 , 0)
 
-    Japanese_food = ('초밥', '라멘', '우동', '돈까스', '오므라이스','X')
-    Japanese_food_calorie = (500, 700, 600, 800, 900)
+    Japanese_food = ('초밥 - 500kcal', '라멘 - 700kcal', '우동 - 600kcal', '돈까스 - 800kcal', '오므라이스 - 900kcal ','X')
+    Japanese_food_calorie = (500, 700, 600, 800, 900, 0)
 
     calorie_add = 0
 
@@ -41,7 +41,7 @@ class Calorie(Routine):
     def Korean(self):
         for index, Food in enumerate(Calorie.Korean_food[0:5]) :
             print(f'{index + 1}. {Food}')
-        k = input("오늘 먹은 음식을 선택하세요 : ")
+        k = input("오늘 먹은 음식을 선택하세요(엔터 누르면 패스) : ")
         if k == '':
             Calorie.calorie_add += 0
         else:
@@ -51,7 +51,7 @@ class Calorie(Routine):
     def Western(self):
         for index, Food in enumerate(Calorie.Western_food[0:5]):
             print(f'{index + 1}. {Food}')
-        w = input("오늘 먹은 음식을 선택하세요 : ")
+        w = input("오늘 먹은 음식을 선택하세요(엔터 누르면 패스) : ")
 
         if w == '':
             Calorie.calorie_add += 0
@@ -62,7 +62,7 @@ class Calorie(Routine):
     def Chinese(self):
         for index, Food in enumerate(Calorie.Chinese_food[0:5]):
             print(f'{index + 1}. {Food}')
-        c = input("오늘 먹은 음식을 선택하세요 : ")
+        c = input("오늘 먹은 음식을 선택하세요(엔터 누르면 패스) : ")
 
         if c == '':
             Calorie.calorie_add += 0
@@ -73,7 +73,7 @@ class Calorie(Routine):
     def Japanese(self):
         for index, Food in enumerate(Calorie.Japanese_food[0:5]):
             print(f'{index + 1}. {Food}')
-        j = input("오늘 먹은 음식을 선택하세요 : ")
+        j = input("오늘 먹은 음식을 선택하세요(엔터 누르면 패스) : ")
         if j == '':
             Calorie.calorie_add += 0
         else:
